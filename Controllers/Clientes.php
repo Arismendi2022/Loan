307fcf25e2bac94324dbfca041b11b62fc34e40d
arismendi2022 <arismendi.guiza@gmail.com>
2023-04-25 10:18:46 -0500

    actualiza controller clientes

--- Controllers/Clientes.php
+++ Controllers/Clientes.php
@@ -26,78 +26,70 @@
 		
 		public function setCliente()
 		{
-//		if($_POST){
-//				if(empty($_POST['txtIdentificacion']) || empty($_POST['txtNombre']) || empty($_POST['txtApellido']) || empty($_POST['txtTelefono']) || empty($_POST['txtEmail']) || empty($_POST['txtNit']) || empty($_POST['txtNombreFiscal']) || empty($_POST['txtDirFiscal']) )
-//				{
-//					$arrResponse = array("status" => false, "msg" => 'Datos incorrectos.');
-//				}else{
-//					$idUsuario = intval($_POST['idUsuario']);
-//					$strIdentificacion = strClean($_POST['txtIdentificacion']);
-//					$strNombre = ucwords(strClean($_POST['txtNombre']));
-//					$strApellido = ucwords(strClean($_POST['txtApellido']));
-//					$intTelefono = intval(strClean($_POST['txtTelefono']));
-//					$strEmail = strtolower(strClean($_POST['txtEmail']));
-//					$strNit = strClean($_POST['txtNit']);
-//					$strNomFiscal = strClean($_POST['txtNombreFiscal']);
-//					$strDirFiscal = strClean($_POST['txtDirFiscal']);
-//					$intTipoId = 7;
-//					$request_user = "";
-//					if($idUsuario == 0)
-//					{
-//						$option = 1;
-//						$strPassword =  empty($_POST['txtPassword']) ? passGenerator() : $_POST['txtPassword'];
-//						$strPasswordEncript = hash("SHA256",$strPassword);
-//						if($_SESSION['permisosMod']['w']){
-//							$request_user = $this->model->insertCliente($strIdentificacion,
-//								$strNombre,
-//								$strApellido,
-//								$intTelefono,
-//								$strEmail,
-//								$strPasswordEncript,
-//								$intTipoId,
-//								$strNit,
-//								$strNomFiscal,
-//								$strDirFiscal );
-//						}
-//					}else{
-//						$option = 2;
-//						$strPassword =  empty($_POST['txtPassword']) ? "" : hash("SHA256",$_POST['txtPassword']);
-//						if($_SESSION['permisosMod']['u']){
-//							$request_user = $this->model->updateCliente($idUsuario,
-//								$strIdentificacion,
-//								$strNombre,
-//								$strApellido,
-//								$intTelefono,
-//								$strEmail,
-//								$strPassword,
-//								$strNit,
-//								$strNomFiscal,
-//								$strDirFiscal);
-//						}
-//					}
-//
-//					if($request_user > 0 )
-//					{
-//						if($option == 1){
-//							$arrResponse = array('status' => true, 'msg' => 'Datos guardados correctamente.');
-//							$nombreUsuario = $strNombre.' '.$strApellido;
-//							$dataUsuario = array('nombreUsuario' => $nombreUsuario,
-//								'email' => $strEmail,
-//								'password' => $strPassword,
-//								'asunto' => 'Bienvenido a tu tienda en línea');
-//							sendEmail($dataUsuario,'email_bienvenida');
-//						}else{
-//							$arrResponse = array('status' => true, 'msg' => 'Datos Actualizados correctamente.');
-//						}
-//					}else if($request_user == 'exist'){
-//						$arrResponse = array('status' => false, 'msg' => '¡Atención! el email o la identificación ya existe, ingrese otro.');
-//					}else{
-//						$arrResponse = array("status" => false, "msg" => 'No es posible almacenar los datos.');
-//					}
-//				}
-//				echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
-//			}
-//			die();
+			if ($_POST) {
+				if (empty($_POST['txtIdentificacion']) || empty($_POST['txtNombre']) || empty($_POST['txtApellido']) || empty($_POST['txtTelefono'])
+					|| empty($_POST['txtEmail']) || empty($_POST['listDepto']) || empty($_POST['listMcpio']) || empty($_POST['listOcup'])
+					|| empty($_POST['dirFiscal'])) {
+					$arrResponse = array("status" => false, "msg" => 'Datos incorrectos.');
+				} else {
+					$idUsuario = intval($_POST['idUsuario']);
+					$strIdentificacion = intval(strClean($_POST['txtIdentificacion']));
+					$strNombre = ucwords(strClean($_POST['txtNombre']));
+					$strApellido = ucwords(strClean($_POST['txtApellido']));
+					$intTelefono = intval(strClean($_POST['txtTelefono']));
+					$strEmail = strtolower(strClean($_POST['txtEmail']));
+					$intlistDepto = intval(strClean($_POST['listDepto']));
+					$intlistMcpio = intval(strClean($_POST['listMcpio']));
+					$intlistMcpio = intval(strClean($_POST['listMcpio']));
+					$intlistOcup = intval(strClean($_POST['listOcup']));
+					$strdirFiscal = intval(strClean($_POST['dirFiscal']));
+					
+					$request_user = "";
+					
+					if ($idUsuario == 0) {
+						$option = 1;
+						if ($_SESSION['permisosMod']['w']) {
+							$request_user = $this->model->insertCliente($strIdentificacion,
+								$strNombre,
+								$strApellido,
+								$intTelefono,
+								$strEmail,
+								$intlistDepto ,
+								$intlistMcpio,
+								$intlistOcup,
+								$strdirFiscal);
+						}
+					} else {
+						/*$option = 2;
+						$strPassword = empty($_POST['txtPassword']) ? "" : hash("SHA256", $_POST['txtPassword']);
+						if ($_SESSION['permisosMod']['u']) {
+							$request_user = $this->model->updateUsuario($idUsuario,
+								$strIdentificacion,
+								$strNombre,
+								$strApellido,
+								$intTelefono,
+								$strEmail,
+								$strPassword,
+								$intTipoId,
+								$intStatus);
+						}*/
+					}
+					if ($option == 1) {
+						if ($request_user != 'exist') {
+							
+							$arrResponse = array('status' => true, 'msg' => 'Datos guardados correctamente.');
+						} elseif ($request_user === 'exist') {
+							$arrResponse = array('status' => false, 'msg' => '¡Atención! el email o la identificación ya existe, ingrese otro.');
+						} else {
+							$arrResponse = array("status" => false, "msg" => 'No es posible almacenar los datos.');
+						}
+					} else {
+						$arrResponse = array('status' => true, 'msg' => 'Datos Actualizados correctamente.');
+					}
+				}
+				echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
+			}
+			die();
 		}
 		
 		// * Selecionamos Departamentos
